6ff55f7fe5761bc9180fe7f8b6243fb174133ca6
asadbekded <asadbek171@gmail.com>
2022-08-25 11:19:18 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var firstName = prompt('Ismingizni kiriting');
+console.log('User Ismi:' +' '+ firstName);
+
+var lastName = prompt('Familiangizni kiriting');
+console.log('User Familiasi:' +' '+ lastName);
+
+var test1 = prompt('Htmlda sarlavhalar uchun neshta teg mavjud a) 4-ta b) 6-ta c) 5-ta')
+console.log('Birinchi test' +' '+ 'User javobi:' + test1 +' '+ 'To\'g\'ri javob:b');
+
+var test2 = prompt('<ul> tegi qanaqa ro\'yxat a) tartibsiz b)tartibli c) ro\'yxat emas')
+console.log('Ikkinchi test' +' '+ 'User javobi:' + test2 +' '+ 'To\'g\'ri javob:a');
+
+var test3 = prompt('Form nima a) ro\'yxat b) sarlavha tegi c) user tomonidan to\'ldiriladigan jadval')
+console.log('Uchinchi test' +' '+ 'User javobi:' + test3 +' '+ 'To\'g\'ri javob:c');
+
+var test4 = prompt('CSSni Htmlga ulashning neshta usuli mavjud a) 2-ta b) 3-ta c) 4-ta')
+console.log('To\'rtinchi test' +' '+ 'User javobi:' + test4 +' '+ 'To\'g\'ri javob:b');
+
+var test5 = prompt('JavaScriptni Htmlga ulashning neshta usuli mavjud a) 2-ta b) 1-ta c) 3-ta')
+console.log('Beshinchi test' +' '+ 'User javobi:' + test5 +' '+ 'To\'g\'ri javob:c');
+
+var test6 = prompt('Ranglari necha xil usul bilan yozsa bo\'ladi a) 3-xil b) 5-xil c) 2-xil')
+console.log('Oltinchi test' +' '+ 'User javobi:' + test6 +' '+ 'To\'g\'ri javob:a');
+
+var test7 = prompt('Fontlani neshta oilasi mavjud a) 3-ta b) 5-ta c) 6-ta')
+console.log('Yettinchi test' +' '+ 'User javobi:' + test7 +' '+ 'To\'g\'ri javob:c');
+
+var test8 = prompt('Position default holatda nima bo\'ladi a) absolute b) static c) fixed')
+console.log('Sakkizinchi test' +' '+ 'User javobi:' + test8 +' '+ 'To\'g\'ri javob:b');
+
+var test9 = prompt('Fontlani nechi xil usulda htmlga ulasak bo\'ladi a) 2-ta b) 4-ta c) 1-ta')
+console.log('To\'qizinchi test' +' '+ 'User javobi:' + test9 +' '+ 'To\'g\'ri javob:a');
+
+var test10 = prompt('Opacity nechidan nechigacha qiymat qabul qiladi a) -2 dan 2 gacha b) 0 dan 1 gacha c) -1 dan 2 gacha')
+console.log('O\'ninchi test' +' '+ 'User javobi:' + test10 +' '+ 'To\'g\'ri javob:b');
